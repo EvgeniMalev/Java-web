9e7457f4c9d76cd92cdd7a14dcbf38ed9c4172c8
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-05-21 23:25:24 +0300

    Create Car.java

---
+++ Spring Fundamentals/Spring Boot Introduction/Car Selection/src/main/java/bg/newspaper/car/specification/Car.java
@@ -0,0 +1,19 @@
+package bg.softuni.car.specification;
+
+public class Car {
+    private String model;
+    private String color;
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
