80ab5b53a698032c5beaf053a057f56b728e70c3
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-05-26 21:40:23 +0300

    Create TvFilmsApplication.java

---
+++ Spring Fundamentals/Spring Boot Introduction/Annotations/Movies/src/main/java/com/example/tvfilms/TvFilmsApplication.java
@@ -0,0 +1,11 @@
+package com.example.tvfilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvFilmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TvFilmsApplication.class, args);
+    }
+}
