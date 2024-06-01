17ba7fc86bad1453c4928589d33d6bbc27712af3
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-06-01 18:24:41 +0300

    Create WorkhoursApplication.java

---
+++ Spring Fundamentals/Spring MVC/work hours/src/main/java/com/example/workhours/WorkhoursApplication.java
@@ -0,0 +1,12 @@
+package com.example.workhours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkhoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkhoursApplication.class, args);
+    }
+}
