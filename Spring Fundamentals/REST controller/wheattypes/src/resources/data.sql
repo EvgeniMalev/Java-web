5a4a1dbbc3551cb2d7ad4bb525bc5dc03fbc0cd7
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-06-03 00:04:34 +0300

    Create data.sql

---
+++ Spring Fundamentals/REST controller/wheattypes/src/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO wheat (type, description) VALUES ('Durum', 'Used for pasta');
+INSERT INTO wheat (type, description) VALUES ('Hard Red Winter', 'Used for bread');
+INSERT INTO wheat (type, description) VALUES ('Soft Red Winter', 'Used for pastries');
