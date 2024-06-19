130ffea8b1b188fad481857b2c2bdf1e80b44f09
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-06-19 23:02:13 +0300

    Create UserController.java

---
+++ Spring Fundamentals/App with Mysql/three/src/main/java/com/three/conn/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(User user) {
+        userService.save(user);
+        return "redirect:/login";
+    }
+}
