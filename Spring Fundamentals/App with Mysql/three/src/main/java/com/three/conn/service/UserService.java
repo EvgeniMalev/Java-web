cf7dfeacbd4b64aa58c36df798b2f3e219053241
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-06-19 23:27:24 +0300

    Create UserService.java

---
+++ Spring Fundamentals/App with Mysql/three/src/main/java/com/three/conn/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
