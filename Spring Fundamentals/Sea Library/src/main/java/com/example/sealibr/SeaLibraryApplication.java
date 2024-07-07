0eefaa9a2f91921dd24cd14405d2c36b8a19bf10
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-07-07 23:12:21 +0300

    Create SeaLibraryApplication.java

---
+++ Spring Fundamentals/Sea Library/src/main/java/com/example/sealibr/SeaLibraryApplication.java
@@ -0,0 +1,11 @@
+package com.example.sealibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeaLibraryApplication.class, args);
+    }
+}
