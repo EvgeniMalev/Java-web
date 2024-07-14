290e79577f9ab91188deefd227bd0cdc1f91653b
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-07-14 13:43:07 +0300

    Create UserDetailsServiceImpl.java

---
+++ Spring Fundamentals/Sea Library/src/main/java/com/example/sealibr/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.sealibrary.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.sealibrary.model.User;
+import com.example.sealibrary.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
