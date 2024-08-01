881c0dc107a83ad1092fef170f9d2b869efc2e88
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-08-01 22:51:33 +0300

    Create Team.java

---
+++ Spring Advanced/Project/soccerapp/src/main/java/com/example/soccerapp/entity/Team.java
@@ -0,0 +1,71 @@
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    private Country country;
+
+    @OneToMany(mappedBy = "team")
+    private List<Player> players;
+
+    @OneToOne
+    private Coach coach;
+
+    @OneToOne
+    private Stadium stadium;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public Coach getCoach() {
+        return coach;
+    }
+
+    public void setCoach(Coach coach) {
+        this.coach = coach;
+    }
+
+    public Stadium getStadium() {
+        return stadium;
+    }
+
+    public void setStadium(Stadium stadium) {
+        this.stadium = stadium;
+    }
+}
