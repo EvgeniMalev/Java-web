e77c751911432cf27a49c696902c6c05ab9e4ffa
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-08-01 22:58:20 +0300

    Create Player.java

---
+++ Spring Advanced/Project/soccerapp/src/main/java/com/example/soccerapp/entity/Player.java
@@ -0,0 +1,45 @@
+import javax.persistence.*;
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String position;
+
+    @ManyToOne
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
