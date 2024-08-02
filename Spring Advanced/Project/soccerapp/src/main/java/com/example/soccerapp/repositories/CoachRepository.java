44e66c13b20ae6924154e6a05391243a49a23dac
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-08-02 21:57:35 +0300

    Delete Spring Advanced/Project/soccerapp/src/main/java/com/example directory

--- Spring Advanced/Project/soccerapp/src/main/java/com/example/soccerapp/repositories/CoachRepository.java
+++
@@ -1,3 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoachRepository extends JpaRepository<Coach, Long> {}
--- Spring Advanced/Project/soccerapp/src/main/java/com/example/soccerapp/repositories/StadiumRepository.java
+++
@@ -1,3 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StadiumRepository extends JpaRepository<Stadium, Long> {}
