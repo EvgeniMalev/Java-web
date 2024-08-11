7cd66d28e3b2784527d921e3214101e5e8d9c16c
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-08-11 16:40:20 +0300

    Create PlayerService.java

---
+++ Spring Advanced/Project/soccerapp/src/main/java/com/soccerapp/service/PlayerService.java
@@ -0,0 +1,13 @@
+package com.soccerapp.service;
+
+import com.soccerapp.model.entity.Player;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PlayerService {
+    Player addPlayer(String name, UUID teamId);
+    void removePlayer(UUID playerId);
+    List<Player> getPlayersByTeam(UUID teamId);
+    void incrementGoals(UUID playerId);
+}
