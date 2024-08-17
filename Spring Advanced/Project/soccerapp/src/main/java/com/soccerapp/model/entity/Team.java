462e4d2c16465fe02dc5cdb98c9efa441f847954
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-08-17 09:20:39 +0300

    Update Team.java

--- Spring Advanced/Project/soccerapp/src/main/java/com/soccerapp/model/entity/Team.java
+++ Spring Advanced/Project/soccerapp/src/main/java/com/soccerapp/model/entity/Team.java
@@ -6,21 +6,29 @@ import java.util.UUID;
 
 @Entity
 public class Team {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     private String name;
 
+    private int goalsScored = 0;
+    private int goalsConceded = 0;
+    private int wins = 0;
+    private int losses = 0;
+    private int draws = 0;
+
     @OneToMany(mappedBy = "homeTeam")
-    private List<Match> homeMatches;
+    private List<Game> homeGames;
 
     @OneToMany(mappedBy = "awayTeam")
-    private List<Match> awayMatches;
+    private List<Game> awayGames;
 
     @OneToMany(mappedBy = "team")
     private List<Player> players;
 
+    // Getters and Setters
 
     public UUID getId() {
         return id;
@@ -38,20 +46,20 @@ public class Team {
         this.name = name;
     }
 
-    public List<Match> getHomeMatches() {
-        return homeMatches;
+    public List<Game> getHomeGames() {
+        return homeGames;
     }
 
-    public void setHomeMatches(List<Match> homeMatches) {
-        this.homeMatches = homeMatches;
+    public void setHomeGames(List<Game> homeGames) {
+        this.homeGames = homeGames;
     }
 
-    public List<Match> getAwayMatches() {
-        return awayMatches;
+    public List<Game> getAwayGames() {
+        return awayGames;
     }
 
-    public void setAwayMatches(List<Match> awayMatches) {
-        this.awayMatches = awayMatches;
+    public void setAwayGames(List<Game> awayGames) {
+        this.awayGames = awayGames;
     }
 
     public List<Player> getPlayers() {
@@ -60,5 +68,58 @@ public class Team {
 
     public void setPlayers(List<Player> players) {
         this.players = players;
+    }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+
+    public int getGoalsConceded() {
+        return goalsConceded;
+    }
+
+    public void setGoalsConceded(int goalsConceded) {
+        this.goalsConceded = goalsConceded;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public void updateStatistics(int goalsScored, int goalsConceded, boolean isHomeTeam) {
+        this.goalsScored += goalsScored;
+        this.goalsConceded += goalsConceded;
+
+        if (goalsScored > goalsConceded) {
+            this.wins++;
+        } else if (goalsScored < goalsConceded) {
+            this.losses++;
+        } else {
+            this.draws++;
+        }
     }
 }
